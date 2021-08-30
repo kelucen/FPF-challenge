c01997ce28176fe82e12e73470a6865789edbb97
Kelly Lucena <lucenalarissa0@gmail.com>
2021-08-30 00:09:57 -0400

    Screen and Logic to edit product, update or delete, were implemented

--- front-end-angular/fpfProducts/src/app/components/product-details/product-details.component.ts
+++ front-end-angular/fpfProducts/src/app/components/product-details/product-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product.model';
+import { ProductService } from 'src/app/services/product.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-details',
@@ -7,9 +10,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailsComponent implements OnInit {
 
-  constructor() { }
+  currentProduct: Product = {
+    name: '',
+    description: '',
+    price:0.0,
+    date:'',
+    category:''
+  };
+
+  message = '';
+  constructor(private productService: ProductService, private route: ActivatedRoute,  private router: Router) { }
 
   ngOnInit(): void {
+    this.message = '';
+    this.getProduct(this.route.snapshot.params.id);
+  }
+  getProduct(id: string): void {
+    this.productService.get(id)
+      .subscribe(
+        data => {
+          this.currentProduct = data;
+          console.log(data);
+        },
+        error => {
+          console.log(error);
+        });
+  }
+  updateProduct(): void {
+    this.message = '';
+
+    this.productService.update(this.currentProduct.id, this.currentProduct)
+      .subscribe(
+        response => {
+          console.log(response);
+          this.message = response.message ? response.message : 'This Product was updated successfully!';
+        },
+        error => {
+          console.log(error);
+        });
   }
 
+  deleteProduct(): void {
+    this.productService.delete(this.currentProduct.id)
+      .subscribe(
+        response => {
+          console.log(response);
+          this.router.navigate(['/products']);
+        },
+        error => {
+          console.log(error);
+        });
+  }
 }

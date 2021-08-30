c8c86332533667b00d0aba9d4d1985b6e71fdc9a
Kelly Lucena <lucenalarissa0@gmail.com>
2021-08-30 00:10:40 -0400

    Screen and Logic to list products were implemented

--- front-end-angular/fpfProducts/src/app/components/product-list/product-list.component.ts
+++ front-end-angular/fpfProducts/src/app/components/product-list/product-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product.model';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-list',
@@ -7,9 +9,68 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductListComponent implements OnInit {
 
-  constructor() { }
+  products?: Product[];
+  currentProduct: Product = {};
+  currentIndex = -1;
+  name = '';  
+
+  constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
+    this.retrieveProducts();
+  }
+
+  retrieveProducts(): void {
+    this.productService.getAll()
+      .subscribe(
+        data => {
+          this.products = data;
+          console.log(data);
+        },
+        error => {
+          console.log(error);
+        });
+  }
+
+  refreshList(): void {
+    this.retrieveProducts();
+    this.currentProduct = {};
+    this.currentIndex = -1;
+  }
+
+  setActiveProduct(product: Product, index: number): void {
+    this.currentProduct = product;
+    this.currentIndex = index;
+  }
+
+  searchName(): void {
+    this.currentProduct = {};
+    this.currentIndex = -1;
+
+    this.productService.findByName(this.name)
+      .subscribe(
+        data => {
+          this.products = data;
+          console.log(data);
+        },
+        error => {
+          console.log(error);
+        });
+  }
+
+  searchCategory(category: any): void {
+    this.currentProduct = {};
+    this.currentIndex = -1;
+
+    this.productService.findByCategory(category)
+      .subscribe(
+        data => {
+          this.products = data;
+          console.log(data);
+        },
+        error => {
+          console.log(error);
+        });
   }
 
 }

1bda9efec65f55bc3c8c6d0f3ec62aed6fec9600
Kelly Lucena <lucenalarissa0@gmail.com>
2021-08-30 00:07:33 -0400

    Configuring global components

--- front-end-angular/fpfProducts/src/app/app-routing.module.ts
+++ front-end-angular/fpfProducts/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddProductComponent } from './components/add-product/add-product.component';
+import { ProductDetailsComponent} from './components/product-details/product-details.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'', redirectTo:'products', pathMatch:'full'},
+  {path:'products', component: ProductListComponent},
+  {path:'product/:id', component: ProductDetailsComponent},
+  {path: 'add', component:AddProductComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- front-end-angular/fpfProducts/src/app/app.module.ts
+++ front-end-angular/fpfProducts/src/app/app.module.ts
@@ -1,26 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { DatePipe } from '@angular/common'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AddTutorialComponent } from './components/add-tutorial/add-tutorial.component';
 import { AddProductComponent } from './components/add-product/add-product.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddTutorialComponent,
     AddProductComponent,
     ProductDetailsComponent,
     ProductListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [DatePipe],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
